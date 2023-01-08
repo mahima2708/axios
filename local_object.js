14cd09ecc8b974fb9a8f73ab6a94da58ac60e3b0
mahima2708 <mhtamta5@gmail.com>
2023-01-08 20:14:31 +0530

    deletion local storage

--- local_object.js
+++ local_object.js
@@ -7,7 +7,6 @@ const userList = document.querySelector('#users');
 myform.addEventListener('submit', onSubmit);
 function onSubmit(e){
     e.preventDefault();
-    console.log(nameInput.value);
     if(nameInput.value ==='' || emailInput.value ==='' || phone_number.value==='')
     {
         msg.classList.add('error');
@@ -17,7 +16,6 @@ function onSubmit(e){
     }
     else{
         const li = document.createElement('li');
-        console.log("##");
         let myObj = {
             nameInput : nameInput.value,
             emailInput : emailInput.value,
@@ -27,12 +25,39 @@ function onSubmit(e){
         let myObj_serialized = JSON.stringify(myObj);
         localStorage.setItem(emailInput.value, myObj_serialized);
         let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+
+          var createEle = document.createElement('input');
+        createEle.type = 'button';
+        createEle.value = "Delete"
+        createEle.id ="buttonid";
+        var createDeletenode = document.createTextNode('Delete');
+        createEle.appendChild(createDeletenode);
       
 
     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}: ${phone_number.value}`));
+       li.appendChild(createEle);
+    li.id = nameInput.value;
     userList.appendChild(li);
+    var buttonid = document.getElementById(nameInput.value);
+    var listid = document.getElementById(nameInput.value);
+    
+      buttonid.addEventListener("click",deleteFunction);
+      function deleteFunction(e){
+        //console.log(listid);
+        // console.log(listid.innerHTML.split(": ")[1]);
+        listid.remove();
+        
+        localStorage.removeItem(listid.innerHTML.split(": ")[1]);
+        
+
+          
+      }
+
+
+
     nameInput.value = '';
     emailInput.value = '';
     phone_number.value = '';
   }
+
 }
